4e0fec44d8ee3eb5a909eed540c854475c32df61
oldo <oldo@telenor.dk>
2014-10-08 21:20:09 +0300

    Extend models Provide mocked repositories for Customers and Passangers

--- AirlinesDemo.Repositories.UnitTests/ContextsTests.cs
+++ AirlinesDemo.Repositories.UnitTests/ContextsTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using AirlinesDemo.Repositories.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AirlinesDemo.Repositories.UnitTests
 {
@@ -6,8 +8,24 @@ namespace AirlinesDemo.Repositories.UnitTests
     public class ContextsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CouldFligthBeInsertedToDb()
         {
+            using (var context = new FlightsContext())
+            {
+                context.Flights.Add(new Flight
+                {
+                    Id = 1,
+                    Description = "Main flight"
+                });
+
+                context.SaveChanges();
+            }
+
+            using (var context = new FlightsContext())
+            {
+                Assert.IsNotNull(context.Flights.FirstOrDefault());
+                Assert.IsTrue(context.Flights.FirstOrDefault().Id == 1);
+            }
         }
     }
 }
